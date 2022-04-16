9f648a5496bde2b9f5f92ec61dee8c2d656c8aec
荒木颯太 <sotaaraki@arakifuutanoMacBook-Pro.local>
2022-04-16 11:53:24 +0900

    修正・最終確認

--- rename_file.py
+++ rename_file.py
@@ -7,37 +7,29 @@
 
 import os
 import glob
-
-#画像ファイルのパスをいじりたい場合はここ
-path_3 = './world_model/dataset/part3/*.jpg'
-path_4 = './world_model/dataset/part4/*.jpg'
-path_5 = './world_model/dataset/part5/*.jpg'
-
-file_list = []
-file_list_3 = sorted(glob.glob(path_3))
-file_list_4 = sorted(glob.glob(path_4))
-file_list_5 = sorted(glob.glob(path_5))
 """
 必要な画像ファイルのpathだけをここで格納する。
 """
 
 
-def append_list(files):
+def append_list(file_list, files):
     for file in files:
-        stem = file.split(".")[1]
-        stem = int(stem.split("_")[2])
+        basename = os.path.basename(file)
+        stem = basename.split(".")[0]
+        stem = int(stem.split("_")[1])
         if stem != 0 and stem != 5:
             file_list.append(file)
 
+    return file_list
 
-path_list = []
-path_4_list = []
+
 """
 画像ファイル名をここで0スタートにしてrenameする。（方角に関してはここでは何もしない）
 """
 
-
-def rename_number(path_list, path_4_list, file_list):
+def rename_number(file_list):
+    path_list = []
+    path_4_list = []
     for file in file_list:
         path_1 = os.path.basename(file)
         path_2 = path_1.split(".")[0]
@@ -47,19 +39,9 @@ def rename_number(path_list, path_4_list, file_list):
         path_5 = path_4.zfill(6)
         path_list.append(path_5)
 
+    return path_list, path_4_list
 
-append_list(file_list_3[879 * 6:])
-append_list(file_list_4)
-append_list(file_list_5[:18])
-rename_number(path_list, path_4_list, file_list)
 
-group_1 = [str(i)
-           for i in range(0, 123)] + [str(i) for i in range(321, 569)
-                                      ] + [str(i) for i in range(1051, 1323)]
-group_2 = [str(i)
-           for i in range(123, 321)] + [str(i) for i in range(569, 765)
-                                        ] + [str(i) for i in range(766, 1051)]
-group_3 = 765
 """
 画像ファイル名の方角をここでrenameする。
 """
@@ -96,13 +78,41 @@ def rename_direction(path_list, path_4_list):
             else:
                 path_list[i] = path_list[i] + "_" + "2"
 
+    return path_list
 
-rename_direction(path_list, path_4_list)
 
-path_list_fin = []
-for i in range(len(path_list)):
-    path__ = "./world_model/dataset/rename_file/" + path_list[i] + ".jpg"
-    path_list_fin.append(path__)
+if __name__ == '__main__':
+    path_3 = './data/raw/part3/*.jpg'
+    path_4 = './data/raw/part4/*.jpg'
+    path_5 = './data/raw/part5/*.jpg'
 
-for i in range(len(file_list)):
-    os.rename(file_list[i], path_list_fin[i])
+    file_list = []
+    file_list_3 = sorted(glob.glob(path_3))
+    file_list_4 = sorted(glob.glob(path_4))
+    file_list_5 = sorted(glob.glob(path_5))
+
+    file_list = append_list(file_list, file_list_3[879 * 6:])
+    file_list = append_list(file_list, file_list_4)
+    file_list = append_list(file_list, file_list_5[:18])
+    path_list, path_4_list = rename_number(file_list)
+
+    group_1 = [str(i) for i in range(0, 123)] + [
+        str(i) for i in range(321, 569)
+    ] + [str(i) for i in range(1051, 1323)]
+    group_2 = [str(i) for i in range(123, 321)] + [
+        str(i) for i in range(569, 765)
+    ] + [str(i) for i in range(766, 1051)]
+    group_3 = "765"
+
+    path_list = rename_direction(path_list, path_4_list)
+
+    new_dir_path = "./data/interim"
+    os.mkdir(new_dir_path)
+
+    path_list_fin = []
+    for i in range(len(path_list)):
+        path__ = new_dir_path + "/" + path_list[i] + ".jpg"
+        path_list_fin.append(path__)
+
+    for i in range(len(file_list)):
+        os.rename(file_list[i], path_list_fin[i])
